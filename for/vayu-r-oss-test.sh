5d4d04011f1fa9c827d371f6fc55135b359a1da5
ZyCromerZ <neetroid97@gmail.com>
2021-05-23 15:56:25 +0700

    up for 'vayu-r-oss-test'
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ for/vayu-r-oss-test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+KernelBranch="main-r"
+
+IncludeFiles "${MainPath}/device/vayu-r-oss.sh"
+CustomUploader="Y"
+DEFFCONFIG="vayu_user_defconfig"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/vayu_kernel"
+FolderUp="keqing-drive"
+TypeBuildTag="[TEST]"
+
+# misc
+# doOsdnUp=$FolderUp
+# doSFUp=$FolderUp
+ 
+
+CloneKernel "--depth=1"
+# CloneGCCOld && CloneGugelClang
+# CompileClangKernel && CleanOut
+CloneCompiledGccTwelve && CloneDTCClang
+CompileClangKernel && CleanOut
+# CloneProtonClang
+# CompileProtonClangKernel && CleanOut
